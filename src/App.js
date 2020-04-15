50b7a1b33b877c185a201a545d2328f05d590dff
uncannyvalle <jvalle0926@gmail.com>
2020-04-15 00:48:45 -0600

    timer works for session

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Parameter from "./components/parameter";
 import Timer from "./components/timer";
@@ -9,33 +9,67 @@ const App = () => {
   //State
   const [breakie, setBreakie] = useState(5);
   const [minutes, setMinutes] = useState(25);
-  const [seconds, setSeconds] = useState("00");
-  const [version, setVersion] = useState("Time Left for Work");
+  const [session, setSession] = useState(minutes);
+  const [timer, setTimer] = useState(25 + ":00");
+  const [seconds, setSeconds] = useState(0);
+  const [running, setRunning] = useState(false);
 
+  const timerButton = () => {
+    running ? setRunning(false) : setRunning(true);
+  };
+
+  useEffect(() => {
+    if (minutes > 10 && seconds === -1 && running) {
+      setSeconds(9);
+      setMinutes(minutes - 1);
+      setTimer(minutes + ":00");
+    } else if (minutes <= 10 && seconds === -1 && running) {
+      setSeconds(9);
+      setMinutes("0" + (minutes - 1));
+    } else if (running && seconds > 10) {
+      setTimeout(() => {
+        setSeconds(seconds - 1);
+        setTimer(minutes + ":" + seconds);
+      }, 1000);
+    } else if (running && seconds >= 0 && seconds <= 10) {
+      setTimeout(() => {
+        setSeconds(seconds - 1);
+        setTimer( minutes + ":0" + seconds);
+      }, 1000);
+    }
+  }, [minutes, seconds, running]);
   //buttons increase and decrease on click
   const decreaseBreakie = () => {
-    if (breakie > 0) {
+    if (breakie > 1) {
       setBreakie(breakie - 1);
     }
   };
   const increaseBreakie = () => {
-    if (breakie > 0) {
+    if (breakie > 1) {
       setBreakie(breakie + 1);
     }
   };
-  const decreaseMinutes = () => {
-    if (minutes > 0) {
-      setMinutes(minutes - 1);
+  const decreaseSession = () => {
+    if (session > 1) {
+      let curr = session - 1;
+      setSession(curr);
+      setMinutes(curr);
     }
   };
-  const increaseMinutes = () => {
-    if (minutes > 0) {
-      setMinutes(minutes + 1);
+  const increaseSession = () => {
+    if (minutes > 1) {
+      let curr = session + 1;
+      setMinutes(curr);
+      setSession(curr);
     }
   };
   const resetNow = () => {
+    setRunning(false);
     setBreakie(5);
     setMinutes(25);
+    setSeconds(0);
+    setTimer(25 + ":00");
+    setSession(25);
   };
 
   return (
@@ -65,17 +99,15 @@ const App = () => {
           decrement="session-decrement"
           increment="session-increment"
           length="session-length"
-          clickLeft={decreaseMinutes}
-          clickRight={increaseMinutes}
+          clickLeft={decreaseSession}
+          clickRight={increaseSession}
         >
-          {minutes}
+          {session}
         </Parameter>
       </div>
-      <Timer vers={version}>
-        {minutes}:{seconds}
-      </Timer>
+      <Timer>{timer}</Timer>
       <div id="buttons">
-        <StartStop />
+        <StartStop clickHandler={timerButton} />
         <Reset clickHandler={resetNow} />
       </div>
     </div>
