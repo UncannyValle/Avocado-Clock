f5cb06b8daea58c5b160b6c71f05b9158862794c
Julian <jvalle0926@gmail.com>
2021-06-06 18:42:06 -0400

    added slight animations

--- src/components/parameter.js
+++ src/components/parameter.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
-const Parameter = (props) => (
-  <div className="parameter">
+import { useSpring, animated, config } from "react-spring";
+
+const Parameter = (props) => {
+  const show = useSpring({
+    opacity: 1,
+    // config: config.molasses,
+    from: {
+      opacity: 0,
+    },
+  });
+  return (
+    <animated.div className="parameter" style={show}>
       <h2 id={props.label}> {props.title}</h2>
       <div className="picker">
         <div id={props.decrement} onClick={props.clickLeft}>
@@ -12,8 +22,9 @@ const Parameter = (props) => (
         <div id={props.increment} onClick={props.clickRight}>
           <FontAwesomeIcon icon={faCaretRight} />
         </div>
-    </div>
-  </div>
-);
+      </div>
+    </animated.div>
+  );
+};
 
 export default Parameter;
